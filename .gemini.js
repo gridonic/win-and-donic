04bd5f2097416b418f2fee49cbe8e1c07425e849
Julien Fierz <jfierz@gmail.com>
2018-01-05 21:15:44 +0100

    Setup support for gemini testing

---
+++ .gemini.js
@@ -0,0 +1,25 @@
+module.exports = {
+    rootUrl: 'http://lo.win-and-donic.ch',
+    gridUrl: 'http://localhost:4444',
+
+    browsers: {
+        chrome: {
+            desiredCapabilities: {
+                browserName: 'chrome',
+                version: '59.0',
+                chromeOptions: {
+                    args: ['headless']
+                }
+            }
+        }
+    },
+
+    system: {
+        plugins: {
+            'html-reporter': {
+                enabled: true,
+                path: 'gemini-report'
+            }
+        }
+    }
+};
---
+++ gemini/index.gemini.js
@@ -0,0 +1,5 @@
+gemini.suite('dashboard', (suite) => {
+    suite.setUrl('/')
+        .setCaptureElements('body')
+        .capture('plain');
+});
